41987cae7b1928cb4c1e54cd149af469f50e8beb
Pratik Kinage <pratikkinage@gmail.com>
2018-05-31 00:03:39 +0530

    Update req.py

--- req.py
+++ req.py
@@ -1,12 +1,3 @@
-# import requests
-# url = 'http://internet.iitb.ac.in/index.php'
-# values = {'uname': '17B030008',
-#           'passwd': 'hihello123@',
-#           "value" : "Login"}
-
-# r = requests.post(url, data=values)
-# print (r.content)
-
 import time
 
 time.sleep(60)
@@ -17,7 +8,7 @@ login_url = 'my_url'
 browser.open('http://internet.iitb.ac.in/index.php')
 form = browser.get_form()
 print(form)
-form["uname"].value = "17B030008" 
-form["passwd"].value = "hihello123@"
+form["uname"].value = "" 
+form["passwd"].value = ""
 print(form)
 browser.submit_form(form)
