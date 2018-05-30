9ee86f283c8f948bdf8381dff0c78472cc3b6d4b
Pratik Kinage <pratikkinage@gmail.com>
2018-05-31 00:06:39 +0530

    Update main_all.py

--- main_all.py
+++ main_all.py
@@ -1,14 +1,18 @@
-import time
+# import time
 
-time.sleep(60)
+# time.sleep(60)
 
 from robobrowser import RoboBrowser
+
+username = ""
+password = ""
+
 browser = RoboBrowser()
 login_url = 'my_url'
 browser.open('http://internet.iitb.ac.in/index.php')
 form = browser.get_form()
 print(form)
-form["uname"].value = "" 
-form["passwd"].value = ""
+form["uname"].value = username
+form["passwd"].value = password
 print(form)
 browser.submit_form(form)
